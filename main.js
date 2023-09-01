fe27e2d23ba5a04a359db8ad0da9e56af2f4784d
Yannick <daantjeyannick@gmail.com>
2023-09-01 12:53:52 +0200

    shool JS

---
+++ main.js
@@ -0,0 +1,30 @@
+let a = 10; 
+let b = 30;
+let result = a + b;
+
+console.log('opdr 1...' , result)
+
+a = 20;
+b = 30;
+result = a + b
+
+console.log('opdr 2...' , result)
+
+a = 25;
+b = 35;
+result = a + b
+
+console.log('opdr 3...' , result)
+
+
+a = 30;
+b = 35;
+result = a + b
+
+console.log('opdr 4...' , result)
+
+a = 45;
+b = 35;
+result = a + b
+
+console.log('opdr 5...' , result)
