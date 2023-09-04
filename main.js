732f34b53da2d05d857fcc96bfe622fdf499c438
Yannick <daantjeyannick@gmail.com>
2023-09-04 18:04:51 +0200

    HW/JS

--- main.js
+++ main.js
@@ -1,68 +1,99 @@
-let a = 10; 
+let a = 10;
 let b = 30;
 let result = a + b;
 
-console.log('opdr 1...1' , result)
+console.log('opdr 1...1', result)
 
 a = 20;
 b = 30;
 result = a + b
 
-console.log('opdr 1...2' , result)
+console.log('opdr 1...2', result)
 
 a = 25;
 b = 35;
 result = a + b
 
-console.log('opdr 1...3' , result)
+console.log('opdr 1...3', result)
 
 
 a = 30;
 b = 35;
 result = a + b
 
-console.log('opdr 1...4' , result)
+console.log('opdr 1...4', result)
 
 a = 45;
 b = 35;
 result = a + b
 
-console.log('opdr 1...5' , result)
+console.log('opdr 1...5', result)
 
 
 let x = 12;
 let y = 13;
-result = x + y 
+result = x + y
 
-console.log('opdr 2...1' , result)
+console.log('opdr 2...1', result)
 
 x = 20;
 y = 30;
-result = x + y 
+result = x + y
 
-console.log('opdr 2...2' , result)
+console.log('opdr 2...2', result)
 
 x = 25;
 y = 35;
-result = x + y 
+result = x + y
 
-console.log('opdr 2...3' , result)
+console.log('opdr 2...3', result)
 
 x = 20;
 y = 30;
-result = x + y 
+result = x + y
 
-console.log('opdr 2...4' , result)
+console.log('opdr 2...4', result)
 
 x = 40;
 y = 50;
-result = x + y 
+result = x + y
 
-console.log('opdr 2...5' , result)
+console.log('opdr 2...5', result)
 
 
 let salary = 2000;
 let raise = 0.05;
 result = salary * raise
 
-console.log('opdr 3...' , result)
+console.log('opdr 3...', result)
+
+x = 4;
+y = 2;
+result = x / y
+
+console.log('opdr 4...1', result)
+
+x = 6;
+y = 2;
+result = x / y
+
+console.log('opdr 4...2', result)
+
+x = 4;
+y = 0;
+result = x / y
+//what i understood it that the computer understend is the 0 result is infinity.
+console.log('opdr 5...', result)
+
+let price = 400.00;
+let intres = 0.025;
+result = price * intres
+
+console.log('opdr 6...', result)
+
+
+price = 100;
+let btw = 1.21;
+result = price * btw
+
+console.log('opdr 7...', result)
