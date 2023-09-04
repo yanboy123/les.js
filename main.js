602201b3cff2dc44686c84353c6af2d0293f62aa
Yannick <daantjeyannick@gmail.com>
2023-09-04 12:03:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,29 +2,67 @@ let a = 10;
 let b = 30;
 let result = a + b;
 
-console.log('opdr 1...' , result)
+console.log('opdr 1...1' , result)
 
 a = 20;
 b = 30;
 result = a + b
 
-console.log('opdr 2...' , result)
+console.log('opdr 1...2' , result)
 
 a = 25;
 b = 35;
 result = a + b
 
-console.log('opdr 3...' , result)
+console.log('opdr 1...3' , result)
 
 
 a = 30;
 b = 35;
 result = a + b
 
-console.log('opdr 4...' , result)
+console.log('opdr 1...4' , result)
 
 a = 45;
 b = 35;
 result = a + b
 
-console.log('opdr 5...' , result)
+console.log('opdr 1...5' , result)
+
+
+let x = 12;
+let y = 13;
+result = x + y 
+
+console.log('opdr 2...1' , result)
+
+x = 20;
+y = 30;
+result = x + y 
+
+console.log('opdr 2...2' , result)
+
+x = 25;
+y = 35;
+result = x + y 
+
+console.log('opdr 2...3' , result)
+
+x = 20;
+y = 30;
+result = x + y 
+
+console.log('opdr 2...4' , result)
+
+x = 40;
+y = 50;
+result = x + y 
+
+console.log('opdr 2...5' , result)
+
+
+let salary = 2000;
+let raise = 0.05;
+result = salary * raise
+
+console.log('opdr 3...' , result)
